6a4a494f8a3f4af61f3f072607e3c81af42f2bbd
Mohammed Rahman <minhaz357@gmail.com>
2020-01-03 02:50:54 -0500

    Home Work(4) updated problem #5
    	-AverageCalculation

--- src/conditional_statements/AverageCalculation.java
+++ src/conditional_statements/AverageCalculation.java
@@ -1,4 +1,5 @@
 package conditional_statements;
+import java.util.Arrays;
 
 /*
 5. Create an array of 5 int values. Calculate the average of those 5 values. [pls don't use for-loop]
@@ -13,33 +14,46 @@ public class AverageCalculation {
     public static void main(String[] args) {
 
         //an array of five int values
-        int[] ia = {2, 3, 4, 5, 6};
+        int[] ia = {2, 3, 4, 25, 60};
 
         //initialization
-        int sum = 0, avg = 0, i = 0;
+        int sum = 0, avg = 0;
+
+        //number of elements in the array
+        int n = ia.length - 1;
+        //last index
+        int j = n;
 
         //sum of array elements
-        sum = ia[0] + ia[1] + ia[2] + ia[3] + ia[4];
-        //average
+        for(int i = 0; i < ia.length; i++){
+
+            sum += ia[i];
+        }
+
+        //calculate average
         avg = sum/5;
+
+        System.out.println("Average: " + avg);
 
         if(avg > 25){
 
-            // Display Array from index 0 to last
-            System.out.println("ia[0]: " + ia[0]);
-            System.out.println("ia[1]: " + ia[1]);
-            System.out.println("ia[2]: " + ia[2]);
-            System.out.println("ia[3]: " + ia[3]);
-            System.out.println("ia[4]: " + ia[4]);
+            System.out.println("Display Array from index 0 to last: ");
+            System.out.println(Arrays.toString(ia));
         }
         else {
 
-            // Display Array from index last to 0
-            System.out.println("ia[4]: " + ia[4]);
-            System.out.println("ia[3]: " + ia[3]);
-            System.out.println("ia[2]: " + ia[2]);
-            System.out.println("ia[1]: " + ia[1]);
-            System.out.println("ia[0]: " + ia[0]);
+            System.out.println( "Display Array from index last to 0: ");
+
+            for(int i = 0; i < n/2; i++){
+                //swap
+                int temp = ia[i];
+                ia[i] = ia[j];
+                ia[j] = temp;
+
+                j--;
+            }
+
+            System.out.println(Arrays.toString(ia));
 
         }
 
