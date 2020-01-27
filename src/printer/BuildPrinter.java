ac923d1d776d2262be53cf50c2fd71a8c2a528c2
Mohammed Rahman <minhaz357@gmail.com>
2020-01-27 11:45:07 -0500

    Home Work(8) on Building A Printer
    	-toner level
    	-printed pages
    	-single sided/double sided printing
    	-printed shteets

---
+++ src/printer/BuildPrinter.java
@@ -0,0 +1,21 @@
+package printer;
+
+public class BuildPrinter {
+
+    public static void main(String[] args) {
+
+        //printer instance
+        Printer printerOne = new Printer();
+        Printer printerTwo = new Printer();
+
+        //print double side
+        printerTwo.setTonerLevel(10);
+        System.out.println("Toner Level: " + printerTwo.getTonerLevel());
+
+        printerTwo.printPage(100, 1, false);
+
+        System.out.println("Number of Printed pages: " + printerTwo.getPrintedPages());
+        System.out.println("Number of Printed sheets: " + printerTwo.getPrintedSheets());
+        System.out.println("Toner Level: " + printerTwo.getTonerLevel());
+    }
+}
